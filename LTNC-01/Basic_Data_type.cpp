51cc21080783dac3717e1cedd34815092357b8fb
Sfyelf <93895697+Sfyelf@users.noreply.github.com>
2024-03-14 09:34:55 +0700

    them bai tap tuan 1

---
+++ LTNC-01/Basic_Data_type.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    
+    scanf("%d %ld %c %f %lf", &a, &b, &c, &d, &e);
+    printf("%d", a);
+    printf("\n");
+    printf("%ld", b);
+    printf("\n");
+    printf("%c", c);
+    printf("\n");
+    printf("%f", d);
+    printf("\n");
+    printf("%lf", e);
+    
+    return 0;
+}
