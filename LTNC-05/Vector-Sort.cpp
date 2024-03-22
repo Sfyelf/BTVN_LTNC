e47eac6192b69e8d2ece2d0e76fbe885af434036
Sfyelf <93895697+Sfyelf@users.noreply.github.com>
2024-03-22 20:55:28 +0700

    update bai 05

---
+++ LTNC-05/Vector-Sort.cpp
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+vector <int> v;
+
+int main() {
+    
+    int n, x;
+    cin >> n;
+    
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> x;
+        v.push_back(x);
+    }
+    
+    sort(v.begin(), v.end());
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
+    
+    return 0;
+}
