fec54d37ef92795d2ea6483a2991793e991e80f0
Sfyelf <93895697+Sfyelf@users.noreply.github.com>
2024-03-22 11:04:51 +0700

    bai 05

---
+++ LTNC-05/Pointer.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void update(int &a, int &b)
+{
+    int a1 = a, b1 = b;
+    a = a1 + b1;
+    b = abs(a1 - b1);
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    update(a, b);
+    cout << a << "\n" << b;
+
+    return 0;
+}
