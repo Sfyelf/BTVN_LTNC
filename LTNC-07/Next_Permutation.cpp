0d5f858022fd78fb5f0b5a1082aa223c357ea9a0
Sfyelf <93895697+Sfyelf@users.noreply.github.com>
2024-04-04 08:23:33 +0700

    play around

---
+++ LTNC-07/Next_Permutation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+vector <char> ch = {'a', 'b', 'c', 'd'};
+
+int main()
+{
+    sort(ch.begin(), ch.end());
+
+    do {
+        cout << ch[0] << " " << ch[1] << " " << ch[2] << "\n";
+    } while (next_permutation(ch.begin(), ch.end()));
+}
---
+++ Space_Invader/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+#include <SDL.h>
+
+using namespace std;
+
+//*****************//
+
+
+
+//*****************//
+
+int main(int argc, char* argv[])
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
